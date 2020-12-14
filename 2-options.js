97be320ef19c069704e32597f92f02ab938613dc
unknown <william4800@hotmail.com>
2020-12-14 17:38:47 -0500

    Commencement fichier html

---
+++ 2-options.js
@@ -0,0 +1,17 @@
+$(function () {
+  $("#accordion-1").accordion({
+    active: 1,
+    animate: {
+      duration: 2000,
+      down: {
+        easing: "easeOutBounce",
+        duration: 2000,
+      },
+    },
+    collapsible: true,
+    header: "h3",
+    heightStyle: "fill", 
+    icons: { header: "ui-icon-plus", activeHeader: "ui-icon-minus" }
+    //disabled: true, event: "mouseover"
+  });
+});
